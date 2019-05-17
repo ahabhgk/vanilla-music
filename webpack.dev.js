3389e9b6188cc03dd10f6544a18884906dca16b9
ahabhgk <ahabhgk@gmail.com>
2019-05-17 22:20:37 +0800

    change dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,11 +10,11 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'), // 配置开发服务运行时的文件根目录
-    port: 3000, // 端口
+    port: 8080, // 端口
     hot: true, // 是否启用热更新
     open: false, // 是否自动打开浏览器
     proxy: { // 配置代理
-      '/api': 'http://localhost:8080',
+      '/api': 'http://localhost:3000',
     },
   },
   module: {
